37294808ae6315d7349739425183ec28b30ffd21
Kate Fang <Kate@MacBook-Air.local>
2020-05-12 10:31:53 -0400

    add geolocation

---
+++ app.js
@@ -0,0 +1,56 @@
+const clientId = "SVJRCTWSPPNDXTKD5QUGB4Q4210QCOM1HVTZQ1XWNQX3ED0B";
+const clientSecret = "5XNAIWKWODNCHLJ5EVTGSZ3EELOKDKXVNQHBOTJ1KKDCGQFJ";
+const v = "20200511";
+const value = document.querySelector("input");
+const form = document.querySelector("form");
+const errorDiv = document.querySelector(".error-message");
+let ll = "new york, ny";
+
+// prompt the user to get current location
+// const getLL = function (e) {
+//   ll = e.coords.latitude + "," + e.coords.longitude;
+//   console.log(ll);
+//   return ll;
+// };
+
+// const error = function (err) {
+//   // console.log(err);
+//   switch (err.code) {
+//     case err.PERMISSION_DENIED:
+//       alert("Location request denied");
+//       break;
+//     case err.TIMEOUT:
+//       alert("Request timed out");
+//       break;
+//     case err.POSITION_UNAVAILABLE:
+//       alert("Location service not available");
+//       break;
+//     default:
+//       alert("An unknown error occurred");
+//       break;
+//   }
+// };
+// navigator.geolocation.getCurrentPosition(getLL, error);
+
+//  using longitude and latitude find businesses nearby
+const findPlaces = async query => {
+  try {
+    const url = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clientSecret}&near=${ll}&v=${v}&query=${query}`;
+    const response = await axios.get(url);
+    let list = response.data.response.groups[0].items;
+    console.log(list);
+
+    // const data = response.data;
+    // console.log(data.groups);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+findPlaces("coffee");
+
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   findPlaces(value);
+//   form.reset();
+// });
