c6d2ee0898fee2d4ca8fee8c68a389a342333294
Kate Fang <Kate@MacBook-Air.local>
2020-05-14 10:40:56 -0400

    latest status

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
-const clientId = config.clientId;
-const clientSecret = config.clientSecret;
-const v = "20200511";
+const key = config.key;
 const value = document.querySelector("#index-input");
 const form = document.querySelector("form");
 const errorDiv = document.querySelector(".error-message");
-let ll = "new york,ny";
+const firstPage = document.querySelector("#first-page");
+const secondPage = document.querySelector("#second-page");
 const listContainer = document.querySelector(".list-container");
+const coffee = document.querySelector("#coffee");
+const bubbleTea = document.querySelector("#bubble-tea");
+const smoothie = document.querySelector("#smoothie");
+const juice = document.querySelector("#juice");
+const loadingDiv = document.querySelector("#loading");
+let ll = "40.730610, -73.935242";
+// let ll;
 
 // prompt the user to get current location
 // const getLL = function (e) {
 //   ll = e.coords.latitude + "," + e.coords.longitude;
 //   console.log(ll);
+//   localStorage.setItem("ll", ll);
+//   console.log(ll);
 //   return ll;
 // };
 
 // const error = function (err) {
-//   // console.log(err);
 //   switch (err.code) {
 //     case err.PERMISSION_DENIED:
 //       alert("Location request denied");
@@ -31,38 +38,126 @@ const listContainer = document.querySelector(".list-container");
 //       break;
 //   }
 // };
-// // might change to watchPosition
-// navigator.geolocation.getCurrentPosition(getLL, error);
 
-//  using longitude and latitude find businesses nearby
-const findPlaces = async query => {
+// window.onload = () => {
+//   if (localStorage.ll || this.ll) {
+//     loadingDiv.remove();
+//   } else {
+//     navigator.geolocation.getCurrentPosition(getLL, error);
+//     loadingDiv.remove();
+//   }
+// };
+
+//  find businesses nearby
+const findPlaces = async keyword => {
   try {
-    const url = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clientSecret}&near=${ll}&v=${v}&query=${query}`;
+    const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${ll}&radius=1500&type=cafe&keyword=${keyword}&key=${key}`;
     const response = await axios.get(url);
-    let list = response.data.response.groups[0].items;
-    showBusiness(list);
+    const data = response.data.results;
+    data.forEach(item => {
+      getDetails(item.place_id);
+    });
   } catch (err) {
     console.log(err);
   }
 };
 
-findPlaces("tea");
+// findPlaces("coffee");
 
-const showBusiness = list => {
-  // window.location.pathname =
-  // "/Users/Kate/GeneralAssembly/SEI/Unit_1/Week_3/find-drinks/list.html";
-
-  list.forEach(item => {
-    console.log(item.venue);
-    const listDiv = document.createElement("div");
-
-    // listDiv.innerHTML = `${item.venue.name}<br>
-    // ${item.venue.location}`;
-    // listContainer.append(listDiv);
-  });
+//get places details
+const getDetails = async placeId => {
+  try {
+    const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,place_id,rating,review,types,vicinity,price_level,photo&key=${key}`;
+    const response = await axios.get(url);
+    displayBusinessInfo(response.data.result);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-// form.addEventListener("submit", e => {
-//   e.preventDefault();
-//   findPlaces(value);
-// });
+// display business details to list.html
+const displayBusinessInfo = item => {
+  console.log(item);
+  let price;
+  switch (item.price_level) {
+    case item.price_level === 1:
+      price = "$";
+      break;
+    case item.price_level === 2:
+      price = "$$";
+      break;
+    case item.price_level === 3:
+      price = "$$$";
+      break;
+    case item.price_level === 4:
+      price = "$$$$";
+      break;
+    case item.price_level === 5:
+      price = "$$$$$";
+      break;
+    case item.price_level > 5:
+      price = "too expensive";
+      break;
+    default:
+      price = "$";
+      break;
+  }
+  const listDiv = document.createElement("div");
+  listDiv.className = "list-wrapper";
+  listDiv.innerHTML = `
+    <img class="venue-img" src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference=${item.photos[0].photo_reference}&key=${key}"/>
+    <div class="content-wrapper">
+    <h4 class="venue-name">${item.name}</h4>
+    <p class="venue-type"><em>${item.types[0]}/${item.types[1]}</em></p>
+    <p class="venue-price">price level: ${price}</p>
+    <p class="venue-rating">rating: ${item.rating}</p>
+    <p class="venue-location">${item.vicinity}</p>
+    </div>
+    `;
+  listContainer.append(listDiv);
+};
+
+// add event handler to each image on front page
+coffee.addEventListener("click", e => {
+  findPlaces("coffee");
+  e.preventDefault();
+});
+
+// if (bubbleTea) {
+//   bubbleTea.addEventListener("click", e => {
+//     e.preventDefault();
+//     findPlaces("bubble tea");
+//     location.href = "list.html";
+//   });
+// }
+
+// if (smoothie) {
+//   smoothie.addEventListener("click", e => {
+//     e.preventDefault();
+//     location.href = "list.html";
+//     findPlaces("smoothies");
+//   });
+// }
+
+// if (juice) {
+//   juice.addEventListener("click", e => {
+//     e.preventDefault();
+//     findPlaces("juices");
+//   });
+// }
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  loadingDiv.style.display = "block";
+  firstPage.style.display = "none";
+  secondPage.style.display = "block";
+  loadingDiv.style.display = "none";
+  removeLastPlace();
+  findPlaces(value);
+});
+
+const removeLastPlace = () => {
+  while (listContainer.lastChild) {
+    listContainer.removeChild(listContainer.lastChild);
+  }
+};
